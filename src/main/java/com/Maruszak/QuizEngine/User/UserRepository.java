57a0c496a4fdfc2a5ca082f9911e766f52dab52e
MarcinMaruszak <MarcinMaruszak@users.noreply.github.com>
2021-02-20 21:31:59 +0000

    clean up

--- src/main/java/com/Maruszak/QuizEngine/User/UserRepository.java
+++ src/main/java/com/Maruszak/QuizEngine/User/UserRepository.java
@@ -1,15 +1,12 @@
 package com.Maruszak.QuizEngine.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.server.ResponseStatusException;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    @Query("SELECT u FROM User u WHERE u.email = :email")
+
     User findByEmail(@Param("email") String email);
 }
